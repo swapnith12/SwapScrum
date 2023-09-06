c28401ffbb8c4f25a49f5b3dfa771d2a45c1ce0b
swapnith <swapnithkondapalli@gmail.com>
2023-09-06 16:35:12 +0530

    TopBar edits done

--- app/components/TopBar.js
+++ app/components/TopBar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import {FcSearch} from 'react-icons/fc'
+import {HiAtSymbol,HiBell} from 'react-icons/hi'
 
 function TopBar() {
   return (
-    <div className='h-12 ml-40 fixed bg-gradient-to-r from-purple-600 to-blue-500 w-full flex items-center justify-around
+    <div className='h-12 ml-40 fixed bg-gradient-to-r from-purple-600 to-blue-500 w-full flex items-center justify-around pl-5 pr-5
     '>
-      <h1 className='flex items-center  text-xl h-12 text-white font-bold '>TopBar</h1>
-      <input type='search' className='items-center'/>
+      <div className='flex items-center'><FcSearch size={24}/>
+      <input type='search' className='text-white items-center placeholder-white bg-transparent ml-1 p-1 focus:ring-0 outline-none' placeholder='search tasks...'/></div>
+      <div className='flex items-center space-x-5'>
+         <HiAtSymbol className='text-white w-6 h-6' />
+         <HiBell className='text-white w-6 h-6'/>
+         <div className='flex items-center space-x-1'>
+          <h4 className='text-white'>Mr.Scrum Master</h4>
+          <img src='https://randomuser.me/api/portraits/med/men/75.jpg' className='rounded-full' width={32}/>
+         </div>
+      </div>
     </div>
   )
 }
